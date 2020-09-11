a87cdee9281d7f7b0e71516031827e7fc76176c7
culturestate <shane@shanecole.com>
2020-09-11 15:47:49 +0800

    typo

--- git-hooks/hash_to_file.sh
+++ git-hooks/hash_to_file.sh
@@ -2,11 +2,11 @@
 
 # Writes the current commit hash to a file so that
 # deployment services like Netlify will rebuild
-# when only files outside of the deploy dir have 
-# changed. 
+# when files outside of the deploy dir have changed. 
 
 # Useful for monorepo development of Netlify build
-# plugns as a pre-commit hook.
+# plugins as a pre-commit hook. See an example at
+# https://github.com/quarva/netlify-plugin-sitrep
 
 # Find the full path to the current repo
 BASE_DIR=$(git rev-parse --show-toplevel)
